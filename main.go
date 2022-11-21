0119808248d611a1eb38f3c9fa7fd5c7b2a9139b
Dylan Rinker <dylan.p.rinker@gmail.com>
2022-11-20 22:30:22 -0500

    first draft

--- main.go
+++ main.go
@@ -1,26 +1,110 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/cli/go-gh"
+	graphql "github.com/cli/shurcooL-graphql"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-membership-audit extension!")
-	client, err := gh.RESTClient(nil)
-	if err != nil {
-		fmt.Println(err)
-		return
+	var organization string
+	var affiliation string
+	var samlIdentities bool
+
+	validAffiliation := []string{"ALL", "OUTSIDE", "DIRECT"}
+
+	flag.StringVar(&organization, "organization", "octodemo", "organization")
+	flag.StringVar(&affiliation, "affiliation", "direCT", "affiliation")
+	flag.BoolVar(&samlIdentities, "saml-identities", false, "saml-identities")
+
+	flag.Parse()
+
+	if !contains(validAffiliation, affiliation) {
+		fmt.Printf("Affiliation value %s is invalid. Valid options for affiliation are All, Direct, and Outside.", affiliation)
+		os.Exit(0)
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
+
+	fmt.Println("Organization: ", organization)
+	fmt.Println("Affiliation: ", affiliation)
+	fmt.Println("SAML Identities: ", samlIdentities)
+
+	OrgMembership(organization, affiliation, samlIdentities)
+	// createFile()
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+// a function that creates a file in the current directory
+func createFile() {
+
+	datetime := getDateTime()
+
+	f, err := os.Create(datetime + "_audit.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	l, err := f.WriteString("test")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+	fmt.Println(l, "bytes written successfully")
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+// a function that gets the current date and time
+func getDateTime() string {
+	t := time.Now()
+	return t.Format("2006-01-02 15:04:05")
+}
+
+// a function that is a graphql query to return all the users in an organization
+func OrgMembership(organization string, affiliation string, samlIdentities bool) {
+	client, err := gh.GQLClient(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var query struct {
+		Organization struct {
+			MembersWithRole struct {
+				Edges []struct {
+					Node struct {
+						Id    string
+						Login string
+					}
+				}
+			} `graphql:"membersWithRole(first: $first)"`
+		} `graphql:"organization(login: $login)"`
+	}
+
+	variables := map[string]interface{}{
+		"first": graphql.Int(10),
+		"login": graphql.String(organization),
+	}
+
+	err = client.Query("OrgMembers", &query, variables)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(query)
+}
+
+// a function to check if a value is part of an array
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if strings.EqualFold(a, e) {
+			return true
+		}
+	}
+	return false
+}
